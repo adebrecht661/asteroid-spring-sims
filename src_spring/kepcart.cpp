7179be111aec570d7286b665ae8bb3dea6ff4279
Alex Debrecht <adebrech@ur.rochester.edu>
2020-04-20 18:29:42 -0400

    Updated kepcart

--- src_spring/kepcart.cpp
+++ src_spring/kepcart.cpp
@@ -1,210 +1,213 @@
 /*
- keplerian conversion routines
- */
-
-/* routines:
- keplerian: returns orbital elements from cartesian coords
- cartesian: returns cartesian from orbital elements
- ecc_ano:   solve Kepler's equation eccentric orbits
- ecc_anohyp:   solve Kepler's equation hyperbolic orbits
+ * Keplerian conversion routines
  */
 
 #include <cmath>
-#include <stdio.h>
-#include <stdlib.h>
+#include "matrix_math.h"
 #include "kepcart.h"
 
-#define SIGN(a) ((a) < 0 ? -1 : 1)
+// Returns orbital elements given the phase space state
+OrbitalElements cart_to_kep(double GM, PhaseState state) {
 
-/* given PhaseState returns orbital elements */
-void cart_to_kep(double GM, PhaseState state, OrbitalElements *orb_el) {
-	double L_x, L_y, L_z, Lsqd, L;
-	double r, vsqd, rdotv, rdot, ecostrueanom, esintrueanom, cosnode, sinnode;
-	double rcosu, rsinu, u, trueanom, eccanom;
+	// Get angular momentum
+	Vector pos = { state.x, state.y, state.z };
+	Vector r_hat = pos / pos.len();
+	Vector vel = { state.v_x, state.v_y, state.v_z };
+	Vector L = cross(pos, vel);
 
-	/* find direction of angular momentum vector */
-	L_x = state.y * state.vz - state.z * state.vy;
-	L_y = state.z * state.vx - state.x * state.vz;
-	L_z = state.x * state.vy - state.y * state.vx;
-	Lsqd = L_x * L_x + L_y * L_y + L_z * L_z;
-	L = sqrt(Lsqd);
+	// Init return values
+	OrbitalElements orb_el;
+	// Calculate orbital inclination
+	orb_el.i = acos(L.getZ() / L.len());
 
-	r = sqrt(state.x * state.x + state.y * state.y + state.z * state.z);
-	vsqd = state.vx * state.vx + state.vy * state.vy + state.vz * state.vz;
+	// Calculate longitude of ascending node
+	if (L.getX() != 0.0 || L.getY() != 0.0) {
+		orb_el.long_asc_node = atan2(L.getX(), -L.getY());
+	} else {
+		orb_el.long_asc_node = 0.0;
+	}
 
-	rdotv = state.x * state.vx + state.y * state.vy + state.z * state.vz;
-	rdot = rdotv / r;
+	// Calculate semimajor axis
+	// Negative if orbit is hyperbolic
+	orb_el.a = 1.0 / (2.0 / pos.len() - pow(vel.len(), 2.0) / GM);
 
-	orb_el->i = acos(L_z / L);
+	// Calculate eccentricity
+	double e_cos_true_anom = pow(L.len(), 2.0) / (GM * pos.len()) - 1.0;
+	double e_sin_true_anom = dot(r_hat, vel) * L.len() / GM;
+	orb_el.e = sqrt(
+			e_cos_true_anom * e_cos_true_anom
+					+ e_sin_true_anom * e_sin_true_anom);
 
-	if (L_x != 0.0 || L_y != 0.0) {
-		orb_el->longnode = atan2(L_x, -L_y);
-	} else
-		orb_el->longnode = 0.0;
+	// Calculate true anomaly
+	double true_anom;
+	if (e_sin_true_anom != 0.0 || e_cos_true_anom != 0.0) {
+		true_anom = atan2(e_sin_true_anom, e_cos_true_anom);
+	} else {
+		true_anom = 0.0;
+	}
 
-	orb_el->a = 1.0 / (2.0 / r - vsqd / GM); // could be negative
+	// Calculate argument of latitude
+	double cos_node = cos(orb_el.long_asc_node);
+	double sin_node = sin(orb_el.long_asc_node);
 
-	ecostrueanom = Lsqd / (GM * r) - 1.0;
-	esintrueanom = rdot * L / GM;
-	orb_el->e = sqrt(ecostrueanom * ecostrueanom + esintrueanom * esintrueanom);
+	double rcosu = pos.getX() * cos_node + pos.getY() * sin_node;
+	if (cos(orb_el.i) == 0.0)
+		throw "You're about to divide by zero.";
+	double rsinu = (pos.getY() * cos_node - pos.getX() * sin_node)
+			/ cos(orb_el.i);
 
-	if (esintrueanom != 0.0 || ecostrueanom != 0.0) {
-		trueanom = atan2(esintrueanom, ecostrueanom);
-	} else
-		trueanom = 0.0;
-
-	cosnode = cos(orb_el->longnode);
-	sinnode = sin(orb_el->longnode);
-
-	/* u is the argument of latitude */
-	rcosu = state.x * cosnode + state.y * sinnode;
-	rsinu = (state.y * cosnode - state.x * sinnode) / cos(orb_el->i);
-	// potential divide by zero here!!!!!!!!
-
+	double u;
 	if (rsinu != 0.0 || rcosu != 0.0) {
 		u = atan2(rsinu, rcosu);
-	} else
-		u = 0.0;
-
-	orb_el->argperi = u - trueanom;
-
-	double foo = sqrt(fabs(1.0 - orb_el->e) / (1.0 + orb_el->e));
-	if (orb_el->e < 1.0) {
-		eccanom = 2.0 * atan(foo * tan(trueanom / 2.0));
-		orb_el->meananom = eccanom - orb_el->e * sin(eccanom);
 	} else {
-		eccanom = 2.0 * atanh(foo * tan(trueanom / 2.0));
-		orb_el->meananom = orb_el->e * sinh(eccanom) - eccanom;
+		u = 0.0;
 	}
-	if (orb_el->argperi > M_PI)
-		orb_el->argperi -= 2.0 * M_PI;
-	if (orb_el->argperi < -M_PI)
-		orb_el->argperi += 2.0 * M_PI;
+
+	// Calculate and shift argument of periapsis
+	orb_el.arg_peri = u - true_anom;
+	if (orb_el.arg_peri > M_PI)
+		orb_el.arg_peri -= 2.0 * M_PI;
+	if (orb_el.arg_peri < -M_PI)
+		orb_el.arg_peri += 2.0 * M_PI;
+
+	// Calculate mean anomaly (and eccentric anomaly)
+	double foo = sqrt(abs(1.0 - orb_el.e) / (1.0 + orb_el.e));
+	double ecc_anom;
+	if (orb_el.e < 1.0) {
+		ecc_anom = 2.0 * atan(foo * tan(true_anom / 2.0));
+		orb_el.mean_anom = ecc_anom - orb_el.e * sin(ecc_anom);
+	} else {
+		ecc_anom = 2.0 * atanh(foo * tan(true_anom / 2.0));
+		orb_el.mean_anom = orb_el.e * sinh(ecc_anom) - ecc_anom;
+	}
+
+	return orb_el;
 }
 
 /* given orbital elements return PhaseState */
 // hyperbolic orbits have e>1, not sure about convention for a positive or not
-void kep_to_cart(double GM, OrbitalElements orb_el, PhaseState *state) {
-	double meanmotion, cosE, sinE, foo;
-	double x, y, z, vx, vy, vz;
-	double xp, yp, zp, xdp, ydp, zdp;
-	double cosw, sinw, cosi, sini, cosnode, sinnode;
-	double E0, rovera;
-	double a = orb_el.a;
-	double e = orb_el.e;
-	double i = orb_el.i;
-	double longnode = orb_el.longnode;
-	double argperi = orb_el.argperi;
-	double meananom = orb_el.meananom;
-	/* double E1, E2, den; */
+PhaseState kep_to_cart(double GM, OrbitalElements orb_el) {
 
-	/* compute eccentric anomaly */
-
-	if (e < 1)
-		E0 = eccentric_anomaly(e, meananom);
-	else
-		E0 = eccentric_anomaly_hyperbolic(e, meananom);
-
-	//  E0 = kepler(e,meananom); // also works
-
-	if (e < 1.0) {
-		cosE = cos(E0);
-		sinE = sin(E0);
+	// Calculate eccentric anomaly
+	double e = orb_el.e, ecc_anom;
+	if (e < 1) {
+		ecc_anom = eccentric_anomaly(e, orb_el.mean_anom);
 	} else {
-		cosE = cosh(E0);
-		sinE = sinh(E0);
+		ecc_anom = eccentric_anomaly_hyperbolic(e, orb_el.mean_anom);
 	}
-	a = fabs(a);
-	meanmotion = sqrt(GM / (a * a * a));
-	foo = sqrt(fabs(1.0 - e * e));
-	/* compute unrotated positions and velocities */
-	rovera = (1.0 - e * cosE);
+
+	// Calculate unrotated position and velocity
+	double cos_ecc, sin_ecc;
+	if (e < 1.0) {
+		cos_ecc = cos(ecc_anom);
+		sin_ecc = sin(ecc_anom);
+	} else {
+		cos_ecc = cosh(ecc_anom);
+		sin_ecc = sinh(ecc_anom);
+	}
+	double a = abs(orb_el.a);
+	double mean_motion = sqrt(GM / pow(a, 3.0));
+	double foo = sqrt(abs(1.0 - pow(e, 2.0)));
+	double r_over_a = (1.0 - e * cos_ecc);
 	if (e > 1.0)
-		rovera *= -1.0;
-	x = a * (cosE - e);
-	y = foo * a * sinE;
-	z = 0.0;
-	vx = -a * meanmotion * sinE / rovera;
-	vy = foo * a * meanmotion * cosE / rovera;
-	vz = 0.0;
+		r_over_a *= -1.0;
+	double x = a * (cos_ecc - e);
 	if (e > 1.0)
 		x *= -1.0;
+	Vector pos = { x, foo * a * sin_ecc, 0.0 };
+	Vector vel = { -a * mean_motion * sin_ecc / r_over_a, foo * a * mean_motion
+			* cos_ecc / r_over_a, 0.0 };
 
-	/* rotate by argument of perihelion in orbit plane*/
-	cosw = cos(argperi);
-	sinw = sin(argperi);
-	xp = x * cosw - y * sinw;
-	yp = x * sinw + y * cosw;
-	zp = z;
-	xdp = vx * cosw - vy * sinw;
-	ydp = vx * sinw + vy * cosw;
-	zdp = vz;
+	// Rotate about z axis by argument of periapsis
+	Matrix rot_peri = getRotMatZ(orb_el.arg_peri);
+	pos = rot_peri * pos;
+	vel = rot_peri * vel;
 
-	/* rotate by inclination about x axis */
-	cosi = cos(i);
-	sini = sin(i);
-	x = xp;
-	y = yp * cosi - zp * sini;
-	z = yp * sini + zp * cosi;
-	vx = xdp;
-	vy = ydp * cosi - zdp * sini;
-	vz = ydp * sini + zdp * cosi;
+	// Rotate about x axis by inclination
+	Matrix rot_inc = getRotMatX(orb_el.i);
+	pos = rot_inc * pos;
+	vel = rot_inc * vel;
 
-	/* rotate by longitude of node about z axis */
-	cosnode = cos(longnode);
-	sinnode = sin(longnode);
-	state->x = x * cosnode - y * sinnode;
-	state->y = x * sinnode + y * cosnode;
-	state->z = z;
-	state->vx = vx * cosnode - vy * sinnode;
-	state->vy = vx * sinnode + vy * cosnode;
-	state->vz = vz;
+	// Rotate about z axis by longitude of ascending node
+	Matrix rot_long = getRotMatZ(orb_el.long_asc_node);
+	pos = rot_long * pos;
+	vel = rot_long * vel;
+
+	// Set and return state vector
+	PhaseState state;
+	state.x = pos.getX();
+	state.y = pos.getY();
+	state.z = pos.getZ();
+	state.v_x = vel.getX();
+	state.v_y = vel.getY();
+	state.v_z = vel.getZ();
+
+	return state;
 }
 
-/* Helpers for coordinate conversion */
+/************/
+/* Helpers  */
+/************/
 
-/* ----------------Solve Kepler's equation
- iterate to get an estimate for eccentric anomaly (u) given the mean anomaly (l).
- Appears to be accurate to level specified, I checked this
- and it works for u,lambda in all quadrants and at high eccentricity
- */
-#define PREC_ecc_ano 1e-16  /* no reason that this must be very accurate in code at present */
+// Solver precision
+#define PREC_ecc_ano 1e-16
+
+// Solve Kepler's equation
+// Iterate to get an estimate for eccentric anomaly (u) given the mean anomaly (l).
+// Works for values in all quadrants and at high eccentricity
 double eccentric_anomaly(double ecc, double mean_anom) {
-	double delta_u, u_guess, l_guess; // mean_anom_guess from the current guess of ecc anom
-	delta_u = 1.0;
-	u_guess = mean_anom + ecc * sin(mean_anom)
+	// Initialize change, guess for eccentric anomaly
+	double delta_u = 1.0;
+	double u_guess = mean_anom + ecc * sin(mean_anom)
 			+ 0.5 * pow(ecc, 2) * sin(2.0 * mean_anom);
-	// also see M+D equation 2.55
-	/* supposed to be good to second order in e, from Brouwer+Clemence
-	 u0 is first guess */
+
+	// Iterate to find eccentric anomaly
+	// From Brouwer+Clemence, also see M+D equations 2.55, 2.58
+	// Good to second order in e
 	int counter = 0;
-	while (fabs(delta_u) > PREC_ecc_ano) {
-		l_guess = u_guess - ecc * sin(u_guess);
+	while (abs(delta_u) > PREC_ecc_ano && counter < 10000) {
+
+		// Current estimate for mean anomaly
+		double l_guess = u_guess - ecc * sin(u_guess);
+
+		// Adjust eccentric anomaly based on current error in mean anomaly
 		delta_u = (mean_anom - l_guess) / (1.0 - ecc * cos(u_guess));
-		u_guess += delta_u; /* this gives a better guess */
+
+		// Update guess at eccentric anomaly
+		u_guess += delta_u;
+
+		// Increment counter
 		counter++;
-		if (counter > 10000)
-			break;
-		// equation 2.58 from M+D
 	}
+
+	// Return best guess for eccentric anomaly
 	return u_guess;
 }
 
-/* ----------------Solve Kepler's equation in hyperbolic case
- iterate to get an estimate for eccentric anomaly (u) given the mean anomaly (l).
- */
+// Solve Kepler's equation in hyperbolic case
+// Iterate to get an estimate for eccentric anomaly (u) given the mean anomaly (l)
 double eccentric_anomaly_hyperbolic(double ecc, double mean_anom) {
-	double delta_u, u_guess, f_guess;
-	delta_u = 1.0;
-	u_guess = log(2.0 * mean_anom / ecc + 1.8); //danby guess
+	// Initialize change, guess for eccentric anomaly
+	double delta_u = 1.0;
+	double u_guess = log(2.0 * mean_anom / ecc + 1.8); // Danby guess
+
+	// Iterate to find eccentric anomaly
 	int counter = 0;
-	while (fabs(delta_u) > PREC_ecc_ano) {
-		f_guess = u_guess - ecc * sinh(u_guess);
-		delta_u = (-mean_anom - f_guess) / (1.0 - ecc * cosh(u_guess));
+	while (abs(delta_u) > PREC_ecc_ano && counter < 10000) {
+
+		// Current guess for mean anomaly
+		double l_guess = u_guess - ecc * sinh(u_guess);
+
+		// Adjust eccentric anomaly based on current error in mean anomaly
+		delta_u = (-mean_anom - l_guess) / (1.0 - ecc * cosh(u_guess));
+
+		// Update guess at eccentric anomaly
 		u_guess += delta_u;
+
+		// Increment counter
 		counter++;
-		if (counter > 10000)
-			break;
 	}
+
+	// Return best guess for eccentric anomaly
 	return u_guess;
 }
--- src_spring/kepcart.h
+++ src_spring/kepcart.h
@@ -1,30 +1,24 @@
 #ifndef _KEPCART_
 #define _KEPCART_
 
-/* Phase space and orbital element structures */
+/*
+ * Phase space and orbital element structures
+ */
 
 // Orbital state in phase space (position, velocity)
 typedef struct PhaseState {
-	double x, y, z, vx, vy, vz;
+	double x, y, z, v_x, v_y, v_z;
 } PhaseState;
 
 // Standard Keplerian elements:
 // Semimajor axis, eccentricity, inclination, longitude of ascending node, argument of periapsis, mean anomaly
 typedef struct OrbitalElements {
-	double a, e, i, longnode, argperi, meananom;
+	double a, e, i, long_asc_node, arg_peri, mean_anom;
 } OrbitalElements;
 
-/* Physical constants
- (may want to move them) */
-
-#define GG 6.6732e-8      // gravitational constant in cgs (cm^3/g/s^2)
-#define CC 2.997924562e10 // speed of light, cm/s
-#define Msol 1.989e33     // mass of sun,  g
-#define AU 1.49597892e13  // astronomical unit, cm
-
-/* Defined in kepcart.c */
-
+/**************************************/
 /* Keplerian to Cartesian conversions */
+/**************************************/
 
 // Cartesian to Keplerian
 void cart_to_kep(double GM, PhaseState state, OrbitalElements *orbel);
