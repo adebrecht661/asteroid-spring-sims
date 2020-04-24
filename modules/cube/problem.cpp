704969b079cd3050f4b1004e77261b18a205eb48
Alex Debrecht <adebrech@ur.rochester.edu>
2020-04-23 20:51:25 -0400

    Clean cube problem

--- modules/cube/problem.cpp
+++ modules/cube/problem.cpp
@@ -30,233 +30,234 @@ extern "C" {
 using std::string;
 using std::vector;
 
-int num_springs;  // number of springs
-vector<spring> springs;  // springs structure
-double surfdist;       // for identifying surface particles
-double Pressure; // vertical pressure
+// Global values
+int num_springs = 0;				// Number of springs
+vector<spring> springs;				// Array of springs
 
-double def_gamma;
-double gamma_fac; // for adjustment of gamma of all springs
-double t_damp;    // end faster damping, relaxation
-double print_interval;   // for printouts
-
-string fileroot;   // output files
-
-int num_perts = 0;
+// Default global values
+int num_perts = 0;					// Number of perturbers
+double def_gamma = 1.0;				// Default spring damping coefficient
+double t_damp = 1.0;				// Time to end increased damping
+double print_interval = 100000.0;	// Interval to print output
+string fileroot = "t1";				// Root name for output files
+double surf_dist = 0.1;	// Distance from surface to count as a surface particle
+double pressure = 0.0;				// Pressure from above
 
 // Forward declarations
-void reb_springs(reb_simulation *const n_body_sim); // to pass springs to display
+void reb_springs(reb_simulation *const n_body_sim);
 void table_top(reb_simulation *const n_body_sim);
 void top_push(reb_simulation *const n_body_sim);
 void heartbeat(reb_simulation *const n_body_sim);
 void additional_forces(reb_simulation *n_body_sim);
 
 int main(int argc, char *argv[]) {
+	// Set up rebound n-body simulation
 	struct reb_simulation *const n_body_sim = reb_create_simulation();
-	// Setup constants
+
+	// Set up rebound constants
 	n_body_sim->integrator = reb_simulation::REB_INTEGRATOR_LEAPFROG;
 	n_body_sim->gravity = reb_simulation::REB_GRAVITY_NONE;
 	n_body_sim->boundary = reb_simulation::REB_BOUNDARY_NONE;
 	n_body_sim->G = 0;
-	n_body_sim->additional_forces = additional_forces; // setup callback function for additional forces
-	double mcube = 1.0;          // total mass of all particles
-	double rcube = 1.0;          // length of cube
-	double tmax = 0.0;           // if 0 integrate forever
+	n_body_sim->additional_forces = additional_forces;
 
-// things to set! ////////////////////// could be read in with parameter file
-	double dt;             // timestep
-	double b_distance;     // mininum interparticle spacing
-	double mush_fac1;       // mush_fac*b_distance is maximum spring length
-	double mush_fac2;       //
-	double mush_fac3;       //
-	def_gamma = 0.01;  // damping parm
-	double ks1 = 0.0;      // spring constant
-	double ks2 = 0.0;      // spring constants
-	double ks3 = 0.0;      // spring constants
+	// Set defaults of other values
+	double cube_mass = 1.0;				// Total mass of all particles
+	double cube_side_len = 1.0;			// Side length of cube
+	double t_max = 0.0;		// Max integration time - if 0, integrate forever
+	double dt = 1e-3;					// Integration timestep
+	double min_part_dist = 0.15;		// Minimum interparticle spacing
+	double max_short_spring_fac = 2.3;// fac*min_part_distance is maximum spring length
+	double max_med_spring_fac = 2.3;// fac*min_part_distance is maximum spring length
+	double max_long_spring_fac = 2.3;// fac*min_part_distance is maximum spring length
+	double gamma_fac = 0.0;	// Factor by which to increase damping until t_damp
+	double k_short = 8e-2;				// Spring constant for short springs
+	double k_med = 8e-2;				// Spring constant for medium springs
+	double k_long = 8e-2;				// Spring constant for long springs
 
-	if (argc == 1) {
-		fileroot = "t1";   // to make output files
-		dt = 1e-3;    // Timestep
-		b_distance = 0.15; // for creating random sphere, min separation between particles
-		mush_fac1 = 2.3; // ratio of smallest spring distance to minimum interparticle dist
-		mush_fac2 = 2.3; // ratio of smallest spring distance to minimum interparticle dist
-		mush_fac3 = 2.3; // ratio of smallest spring distance to minimum interparticle dist
-		// spring damping
-		def_gamma = 1.0;    // initial factor for initial damping value for springs
-		t_damp = 1.0;    // gamma to final values for all springs at this time
-		ks1 = 8e-2;   // spring constant
-		ks2 = 8e-2;   // spring constant
-		ks3 = 8e-2;   // spring constant
-		print_interval = 100000.0;  // printouts
-		surfdist = 0.1;         // for identifying surface particles
-		Pressure = 0.0;         // apply presure
-	} else {
-		FILE *fpi; // read in a parameter file
-		fpi = fopen(argv[1], "r");
-		char line[300];
-		char froot[100];
-		fgets(line, 300, fpi);
-		sscanf(line, "%s", froot);
-		fileroot = froot;
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &dt);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &tmax);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &print_interval);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &b_distance);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &mush_fac1);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &mush_fac2);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &mush_fac3);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &ks1);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &ks2);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &ks3);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &def_gamma);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &gamma_fac);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &t_damp);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &surfdist);
-		fgets(line, 300, fpi);
-		sscanf(line, "%lf", &Pressure);
+	// Read in a parameter file, if one is provided
+	// Caution: no real error checking
+	if (argc > 1) {
+		std::ifstream param_file(argv[1], std::ios::in);
+		param_file >> fileroot;
+		param_file >> dt;
+		param_file >> t_max;
+		param_file >> print_interval;
+		param_file >> min_part_dist;
+		param_file >> max_short_spring_fac;
+		param_file >> max_med_spring_fac;
+		param_file >> max_long_spring_fac;
+		param_file >> k_short;
+		param_file >> k_med;
+		param_file >> k_long;
+		param_file >> def_gamma;
+		param_file >> gamma_fac;
+		param_file >> t_damp;
+		param_file >> surf_dist;
+		param_file >> pressure;
 
-		printf("parm file read in\n");
-
+		std::cout << "Read in " << argv[1] << std::endl;
 	}
 
-/// end of things to set /////////////////////////
+	// Set simulation parameters based on input
+	n_body_sim->dt = dt;								// Integration timestep
+	const double box_size = 1.1 * cube_side_len;		// Display window size
+	reb_configure_box(n_body_sim, box_size, 1, 1, 1);// Last three terms are number of root boxes in x,y,z directions
+	n_body_sim->softening = min_part_dist / 100.0;// Gravitational softening length
 
-	n_body_sim->dt = dt;            // set integration timestep
-	const double boxsize = 1.1 * rcube;    // display window
-	reb_configure_box(n_body_sim, boxsize, 1, 1, 1);
-	n_body_sim->softening = b_distance / 100.0;	// Gravitational softening length
-// viewer +x to right, +y to up, z back and forth along line of sight
+	// Set default spring parameters
+	spring def_spring;
+	def_spring.gamma = def_gamma;
+	def_spring.k = k_short;
+	def_spring.k_heat = 1.0;
+	double short_spring_dist = min_part_dist * max_short_spring_fac;
+	double med_spring_dist = min_part_dist * max_med_spring_fac;
+	double long_spring_dist = min_part_dist * max_long_spring_fac;
 
-	struct spring spring_mush; // spring parameters for mush
-	// properties of springs
-	spring_mush.gamma = def_gamma; // damping coefficient
-	spring_mush.k = ks1; // spring constant
-	spring_mush.k_heat = 1.0;    // heat transport coefficient
-	double mush_distance1 = b_distance * mush_fac1;
-	double mush_distance2 = b_distance * mush_fac2;
-	double mush_distance3 = b_distance * mush_fac3;
-	// distance for connecting and reconnecting springs
+	// Filename for info output
+	string filename;
+	filename = fileroot + "_run.txt";
 
-	char fname[200];
-	sprintf(fname, "%s_run.txt", fileroot.c_str()); // for simulation info
+	// Create rectangular particle distribution
+	rand_rectangle(n_body_sim, min_part_dist, cube_side_len, cube_side_len,
+			cube_side_len, cube_mass);
 
-	num_springs = 0; // start with no springs
+	// Connect particles within interparticle distance by springs
+	// Short springs
+	connect_springs_dist(n_body_sim, short_spring_dist, 0, n_body_sim->N,
+			def_spring);
+	// Medium springs
+	def_spring.k = k_med;
+	connect_springs_dist(n_body_sim, med_spring_dist, 0, n_body_sim->N,
+			def_spring);
+	// Long springs
+	def_spring.k = k_long;
+	connect_springs_dist(n_body_sim, long_spring_dist, 0, n_body_sim->N,
+			def_spring);
 
-	// create particle distribution
+	// Pass springs to display
+	reb_springs(n_body_sim);
 
-	rand_rectangle(n_body_sim, b_distance, rcube, rcube, rcube, mcube);
+	// Start with enhanced damping
+	set_gamma(def_gamma * gamma_fac);
 
-	// make springs, all pairs connected within interparticle distance mush_distance
-	connect_springs_dist(n_body_sim, mush_distance1, 0, n_body_sim->N, spring_mush);
-	spring_mush.k = ks2; // add longer springs!
-	connect_springs_dist(n_body_sim, mush_distance2, 0, n_body_sim->N, spring_mush);
-	spring_mush.k = ks3; // add longer springs!
-	connect_springs_dist(n_body_sim, mush_distance3, 0, n_body_sim->N, spring_mush);
-
-	reb_springs(n_body_sim); // pass spring index list to display
-	set_gamma(def_gamma*gamma_fac);  // start with enhanced damping
-
+	// Set function called at every integration timestep
 	n_body_sim->heartbeat = heartbeat;
 
-	if (tmax == 0.0) // start the integration!!!!
+	// Integrate simulation
+	if (t_max == 0.0) {
 		reb_integrate(n_body_sim, INFINITY);
-	else
-		reb_integrate(n_body_sim, tmax);
+	} else {
+		reb_integrate(n_body_sim, t_max);
+	}
 }
 
-void heartbeat(struct reb_simulation *const r) {
-	static int index = 0;
-	if (reb_output_check(r, 10.0 * r->dt)) {
-		reb_output_timing(r, 0); // print time of simulation run on screen
+// Things to happen every integration timestep
+void heartbeat(struct reb_simulation *const n_body_sim) {
+
+	// Output rebound timing info every 10 timesteps
+	if (reb_output_check(n_body_sim, 10.0 * n_body_sim->dt)) {
+		reb_output_timing(n_body_sim, 0);
 	}
-	if (abs(r->t - t_damp) < 0.9 * r->dt) {
+
+	// Turn off damping at t_damp
+	if (abs(n_body_sim->t - t_damp) < 0.9 * n_body_sim->dt) {
 		set_gamma(def_gamma);
 	}
-	// damp initial bounce only , end damping
-	// reset gamma only at t near t_damp
 
-	// stuff to do every timestep
-	// nothing!
-
-	if (reb_output_check(r, print_interval)) {
-		write_particles(r, fileroot, index); //   output particle positions
-		index++;
+	// Output particle info every print interval
+	if (reb_output_check(n_body_sim, print_interval)) {
+		write_particles(n_body_sim, fileroot,
+				(int) n_body_sim->dt / print_interval);
 	}
-
 }
 
+// Initialize accelerations (because not using rebound gravity) and add forces other than gravity
 void additional_forces(reb_simulation *n_body_sim) {
+	// Init accelerations
 	zero_accel(n_body_sim);
-	spring_forces(n_body_sim); // spring forces
-	top_push(n_body_sim); // top force
-	table_top(n_body_sim); // bottom force
+
+	// Springs
+	spring_forces(n_body_sim);
+
+	// Pressure from top
+	top_push(n_body_sim);
+
+	// Sitting on a table
+	table_top(n_body_sim);
 }
 
-// make a spring index list to pass to viewer
-void reb_springs(struct reb_simulation *const r) {
-	r->NS = num_springs;
-	r->springs_i = (int*)malloc(num_springs * sizeof(int));
-	r->springs_j = (int*)malloc(num_springs * sizeof(int));
+// Make a spring index list to pass to viewer
+void reb_springs(struct reb_simulation *const n_body_sim) {
+	// Set rebound spring info
+	n_body_sim->NS = num_springs;
+	n_body_sim->springs_i = (int*) malloc(num_springs * sizeof(int));
+	n_body_sim->springs_j = (int*) malloc(num_springs * sizeof(int));
+
+	// Add each spring to arrays
 	for (int i = 0; i < num_springs; i++) {
-		r->springs_i[i] = springs[i].particle_1;
-		r->springs_j[i] = springs[i].particle_2;
+		n_body_sim->springs_i[i] = springs[i].particle_1;
+		n_body_sim->springs_j[i] = springs[i].particle_2;
 	}
 }
 
-// I need to mark top surface in the first call as it can move!
+// Apply pressure to top of cube
+// Mark top surface in the first call, since it can move
 void top_push(struct reb_simulation *const n_body_sim) {
-	static int *surflist; // list of top surface  particles!
-	static int Nsurf = 0;  // number of particles on top surface
-	static int first = 0;
-	if (first == 0) { // first time the routine is called
-		first = 1;
-		surflist = (int*)malloc(sizeof(int) * n_body_sim->N);
+	// Get particle info
+	reb_particle *particles = n_body_sim->particles;
+
+	// List and number of top surface particles
+	static vector<int> surf_list(n_body_sim->N, -1);
+	static int N_surf = 0;
+
+	// Mark surface particles at first run
+	static bool first = true;
+	if (first) {
+		first = false;
 		for (int i = 0; i < n_body_sim->N; i++) {
-			if (n_body_sim->particles[i].y > 0.5 - surfdist) {
-				surflist[Nsurf] = i;
-				Nsurf++;
+			if (particles[i].y > 0.5 - surf_dist) {
+				surf_list[N_surf] = i;
+				N_surf++;
 			}
 		}
-		printf("Nsurf top =%d\n", Nsurf);
+		std::cout << "There are " << N_surf
+				<< " particles on the top of the cube." << std::endl;
 	}
-	for (int j = 0; j < Nsurf; j++) {
-		int i = surflist[j];
-		n_body_sim->particles[i].ay -= Pressure / Nsurf
-				/ n_body_sim->particles[i].m;
+
+	// Apply pressure to surface particles
+	for (int j = 0; j < N_surf; j++) {
+		int i = surf_list[j];
+		particles[i].ay -= pressure / N_surf / particles[i].m;
 	}
 }
 
-void table_top(struct reb_simulation *const r) {
-	static int *surflist; // list of bottom surface  particles!
-	static int Nsurf = 0;  // number of particles on bottom surface
-	static int first = 0;
-	if (first == 0) { // first time the routine is called
-		first = 1;
-		surflist = (int*)malloc(sizeof(int) * r->N);
-		for (int i = 0; i < r->N; i++) {
-			if (r->particles[i].y < -0.5 + surfdist) {
-				surflist[Nsurf] = i;
-				Nsurf++;
+// Cube can't move through table
+void table_top(struct reb_simulation *const n_body_sim) {
+	// Get particle info
+	reb_particle *particles = n_body_sim->particles;
+
+	// List and number of top surface particles
+	static vector<int> surf_list(n_body_sim->N, -1);
+	static int N_surf = 0;
+
+	// Mark surface particles at first run
+	static bool first = true;
+	if (first) {
+		first = false;
+		for (int i = 0; i < n_body_sim->N; i++) {
+			if (particles[i].y < -0.5 + surf_dist) {
+				surf_list[N_surf] = i;
+				N_surf++;
 			}
 		}
-		printf("Nsurf bottom=%d\n", Nsurf);
+		std::cout << "There are " << N_surf
+				<< " particles on the bottom of the cube." << std::endl;
 	}
-	for (int j = 0; j < Nsurf; j++) {
-		int i = surflist[j];
-		r->particles[i].ay = 0.0;
+
+	// Stop particles from accelerating
+	for (int j = 0; j < N_surf; j++) {
+		int i = surf_list[j];
+		particles[i].ay = 0.0;
 	}
 }
